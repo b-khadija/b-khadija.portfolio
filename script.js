38219ea686b8c00aa2ea2bf033784e32aaf37d66
b-khadija <khadija.bayssir@gmail.com>
2023-09-03 09:28:47 +0200

    Added new images change style animation with gsap

--- script.js
+++ script.js
@@ -29,3 +29,106 @@ function closeMenuMobile() {
   document.querySelector(".navigation").classList.remove("open");
   document.querySelector(".overlay_menu_mobile").classList.remove("open");
 }
+
+//Scroll GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+function updateScrollTriggers() {
+  ScrollTrigger.refresh();
+}
+
+// Ajoutez un événement de redimensionnement de la fenêtre
+window.addEventListener("resize", updateScrollTriggers);
+
+// Ajoutez un événement de défilement vers le haut (dézoom)
+window.addEventListener("scroll", updateScrollTriggers);
+
+gsap.to(".image-container",{
+  scale: 52,
+  ease: "ease",
+  scrollTrigger:{
+    trigger: ".video-section",
+    scrub: 1,
+    start: "top top",
+    end: "bottom",
+    pin: true
+  }
+})
+
+gsap.to('.right' ,{
+  autoAlpha:0,
+  x:500,
+  duration:1.5,
+  scrollTrigger:{
+    start:1
+  }
+})
+
+gsap.to('.left' ,{
+  autoAlpha:0,
+  x:-500,
+  duration:1.5,
+  scrollTrigger:{
+    start:1
+  }
+})
+
+gsap.to('.txt-bottom',{
+  autoAlpha:0,
+  letterSpacing:-10,
+  duration:2,
+  scrollTrigger:{
+    start:2
+  }
+})
+
+const tl = gsap.timeline();
+tl.from('.left-side div',{
+  y:150,
+  opacity:0,
+  stagger:{
+    amount:.4
+  },
+  delay:.5
+}).from('.right-side',{opacity:0,duration:2},.5)
+.to('.wrapper' ,{x:-window.innerWidth})
+
+
+ScrollTrigger.create({
+  animation:tl,
+  trigger:'.wrapper',
+  start:"top top",
+  end:"+=600",
+  scrub:1,
+  pin:true,
+  ease:"ease"
+})
+
+gsap.utils.toArray('.col').forEach(image=>{
+  gsap.fromTo(image,{
+    opacity:.3,
+    x:0
+  },{
+    opacity:1,
+    x:-50,
+    scrollTrigger:{
+      trigger:image,
+      start:"10%",
+      stagger:{
+        amount:.4
+      }
+    }
+  })
+})
+
+const timeline = gsap.timeline();
+
+timeline.from('.title span', {
+  y: 150,
+  skewY: 7,
+  duration: 3
+}).from('.txt-bottom',{
+  letterSpacing:-10,
+  opacity:0,
+  duration:3
+})
