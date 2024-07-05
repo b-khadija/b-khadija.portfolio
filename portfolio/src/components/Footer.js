45c51eecfd328863db8cb04bf6f844b3f70dcd49
b-khadija <khadija.bayssir@gmail.com>
2024-07-05 22:48:07 +0200

    FEAT: Created footer

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 text-center w-full">
+      <p>© 2024 - Tous droits réservés</p>
+    </footer>
+  );
+};
+
+export default Footer;
