632060d96c1881d08d116875edf3e4858a0112bf
b-khadija <khadija.bayssir@gmail.com>
2024-07-18 20:17:43 +0200

    CHORE: Update mobile styles and add modal backdrop filter

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 text-center w-full">
+    <footer className="fixed bottom-0 left-0 w-full text-center p-4 lg:hidden">
       <p>© 2024 - Tous droits réservés</p>
     </footer>
   );
